d95345a43cd1af7017209061443b4b081d50f7c8
Joern Rennecke <joern.rennecke@embecosm.com>
2013-02-16 14:59:56 +0000

    gcc:         * config/arc/arc.md (mrgb_i, mrgb_i+1, mrgb_i+2): New patterns. gcc/testsuite:         * gcc.target/arc/mrgb_i-1.c: New file.

---
+++ gcc/testsuite/gcc.target/arc/mrgb_i-1.c
@@ -0,0 +1,13 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -mbitops" } */
+
+struct { unsigned a: 5, b: 8, c: 8, d: 11; } foo;
+struct { unsigned a: 3, b: 8, c: 21; } bar;
+
+void
+f (void)
+{
+  foo.b = foo.c;
+  foo.c = bar.b;
+}
+/* { dg-final { scan-assembler "mrgb_i\[ \t\]+r\[0-5\]+, *r\[0-5\]+, *r\[0-5\]+, *5, *13, *8, *13, *3, *8" } } */
