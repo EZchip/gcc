abc06b5ce2155faaeb8ea2f5d7cb71512f2fa98c
Joern Rennecke <joern.rennecke@embecosm.com>
2013-04-04 15:48:49 +0100

    * gcc.target/arc/setmem-2.c, gcc.target/arc/setmem-3.c: New files.         * gcc.target/arc/setmem-4.c: Likewise.

---
+++ gcc/testsuite/gcc.target/arc/setmem-2.c
@@ -0,0 +1,18 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -munaligned-access" } */
+
+#include <string.h>
+
+void
+f (char *d)
+{
+  static const char a[] =
+{
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+  'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+  'u', 'v', 'w', 'x', 'y', 'z',
+};
+  memcpy (d, a, 20);
+}
+/* { dg-final { scan-assembler-not "stb"  } } */
+/* { dg-final { scan-assembler-not "memcpy"  { xfail *-*-* } } } */
---
+++ gcc/testsuite/gcc.target/arc/setmem-3.c
@@ -0,0 +1,12 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -munaligned-access" } */
+
+#include <string.h>
+
+void
+f (char *d)
+{
+  const char a[26] = "abcdefghijklmnopqrstuvwxyz";
+  memcpy (d, a, 20);
+}
+/* { dg-final { scan-assembler-not "stb"  } } */
